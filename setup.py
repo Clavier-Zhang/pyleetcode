f138d2672315d28275d3a1601ed37b6eb640ff5a
Clavier-Zhang <y2595zha@edu.uwaterloo.ca>
2019-02-16 10:00:45 -0500

    update README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyleetcode',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
